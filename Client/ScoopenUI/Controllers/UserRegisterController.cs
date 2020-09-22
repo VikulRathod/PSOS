90cf9eb6735d6442517a88cfe85ba1a25cad7d22
Rajendra-Hande <raj.hande.net@gmail.com>
2020-09-22 16:46:54 +0530

    User Register form
    user register form 22/09/2020

---
+++ Client/ScoopenUI/Controllers/UserRegisterController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ScoopenAPIModals.Account
+
+
+namespace RegistrationAndLogin.Controllers
+{
+    public class UserRegisterController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(ModelUserClass user)
+        {
+            if (ModelState.IsValid)
+            { }
+            return View();
+        }
+
+    }
+}
