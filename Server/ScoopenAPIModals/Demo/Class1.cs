eb718dcfbae900b2b95f0c8fb86116716f9d73ba
ADVAIT <ADVAIT@DESKTOP-MGNP7IC>
2020-09-26 12:43:55 +0530

    add properise class1

---
+++ Server/ScoopenAPIModals/Demo/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScoopenAPIModals.Demo
+{
+    class Class1
+    {
+        public int Id { set; get; }
+        public string name { set; get; }
+         public string gender { set; get; }
+    }
+}
