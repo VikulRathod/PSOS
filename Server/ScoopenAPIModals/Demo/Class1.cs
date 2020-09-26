b75bc8a01de467771d2988f44221741be6d687e3
ADVAIT <ADVAIT@DESKTOP-MGNP7IC>
2020-09-26 12:58:37 +0530

    add one more prop

--- Server/ScoopenAPIModals/Demo/Class1.cs
+++ Server/ScoopenAPIModals/Demo/Class1.cs
@@ -11,5 +11,7 @@ namespace ScoopenAPIModals.Demo
         public int Id { set; get; }
         public string name { set; get; }
          public string gender { set; get; }
+
+        public int mobilenumber { set; get; }
     }
 }
